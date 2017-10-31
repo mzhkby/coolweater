526a233e20f6d269214235a272e219be10c154d7
908391541@qq.com <mzh19920528kby!>
2017-10-31 21:07:09 +0800

    First commit.

---
+++ app/src/main/java/www/it4pl/com/coolweater/MainActivity.java
@@ -0,0 +1,13 @@
+package www.it4pl.com.coolweater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
